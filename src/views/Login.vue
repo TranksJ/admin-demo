23d5e4ff808f385306924af8d8cfd2c5e923f3fd
jzp <2553216090@qq.com>
2020-10-14 21:57:41 +0800

    新建登陆页面

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="login">
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+<style lang="less" scoped>
+    .login{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        width: 500px;
+        height:400px;
+        border: 1px solid #DDDDDD;
+        border-radius: 5px;
+        background-color: #FFFFFF;
+    }
+</style>
