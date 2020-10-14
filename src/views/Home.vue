23149e0683215d12343b7ed62df8f3e248c4a83c
jzp <2553216090>
2020-10-14 17:28:58 +0800

    初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 头部导航栏 -->
+    <div class="header"></div>
+    <!--中间部分-->
+    <div class="content">
+      <!-- 侧边栏 -->
+      <div class="aside"></div>
+      <!-- 主体内容 -->
+      <div class="main"></div>
+    </div>
+    <!-- 页脚 -->
+    <div class="footer"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+<style lang="less" scoped>
+.home{
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.header{
+  height: 60px;
+  width: 100%;
+  background-color: #409EFF;
+}
+.content{
+  flex: 1;
+  width: 100%;
+  display: flex;
+  .aside{
+    width: 200px;
+    height: 100%;
+  }
+  .main{
+    height: 100%;
+    flex: 1;
+  }
+}
+.footer{
+  height: 40px;
+  width: 100%;
+}
+</style>
